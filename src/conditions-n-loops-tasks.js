2198b8544681f0cc8af4b9a3247985dc426e03d0
Yana <bojcenko.jana@gmail.com>
2024-01-23 20:34:18 +0100

    feat: implement the function getMaxNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,15 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let maxNumber = a;
+  if (maxNumber < b) {
+    maxNumber = b;
+  }
+  if (maxNumber < c) {
+    maxNumber = c;
+  }
+  return maxNumber;
 }
 
 /**
